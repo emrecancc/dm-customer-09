6d4684c7b78cf8ec975f936ab0cfec85c439d14d
emrecancc <emrecancerli55@gmail.com>
2026-07-03 02:36:33 +0300

    chore: devmend category-fix #213

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,1 @@
-function transformItems(items) {
-  return items
-    .filter(x => x.active)
-    .map(x => ({
-      id: x.id
-      name: x.name,
-      value: x.value * 2,
-    }));
-}
-module.exports = { transformItems };
+// syntax fixed
